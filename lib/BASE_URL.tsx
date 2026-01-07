ed55564d453786ed5927dee207f32b6b20f7d103
AvazxonAD <asqarovavazxon77@gmail.com>
2026-01-07 14:42:45 +0500

    update base url

--- lib/BASE_URL.tsx
+++ lib/BASE_URL.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://178.209.127.90:5000/api";
-export const API_BASE_URL = "http://178.209.127.90:5000";
+export const BASE_URL = "https://raqamli-manaviyat.uz/api/teacher-back";
+export const API_BASE_URL = "https://raqamli-manaviyat.uz/api/teacher-back";
